c8e0f1eced36c3b1f11fb54fcd9d539d68b4a410
Simon Burton <burt76@outlook.com>
2021-01-19 18:10:39 +0000

    add

--- deploy.bat
+++
@@ -1,3 +0,0 @@
-
-cd C:\Octopus\Applications\DEV\Archimedes.Service.Broker.Node\Release
-pm2 start app.js
---
+++ deploy.ps1
@@ -0,0 +1,2 @@
+﻿cd C:\Octopus\Applications\DEV\Archimedes.Service.Broker.Node\Release
+C:\Users\burt7\AppData\Roaming\npm\pm2.cmd restart app.js
